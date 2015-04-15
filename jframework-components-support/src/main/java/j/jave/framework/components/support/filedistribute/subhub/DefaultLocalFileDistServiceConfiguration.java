92cc1968958885a50de5106670e4e4ed1d3e1ea1
jinwanmei <3cgg@163.com>
2015-04-16 00:26:08 +0800

    end

---
+++ jframework-components-support/src/main/java/j/jave/framework/components/support/filedistribute/subhub/DefaultLocalFileDistServiceConfiguration.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package j.jave.framework.components.support.filedistribute.subhub;
+
+import j.jave.framework.filedistribute.JDefaultLocalFileDistServiceConfigure;
+import j.jave.framework.filedistribute.JDefaultLocalFilePathStrategy;
+
+/**
+ * @author J
+ */
+public class DefaultLocalFileDistServiceConfiguration implements
+		JDefaultLocalFileDistServiceConfigure {
+
+	private String localDirectory="D:/java_/server-directory/file" ;
+
+	private  JDefaultLocalFilePathStrategy jDefaultLocalFilePathStrategy=new JDefaultLocalFilePathStrategy();
+	
+	/* (non-Javadoc)
+	 * @see j.jave.framework.filedistribute.JDefaultLocalFileDistServiceConfigure#setLocalDirectory(java.lang.String)
+	 */
+	@Override
+	public void setLocalDirectory(String localDirectory) {
+		this.localDirectory=localDirectory;
+	}
+
+	/* (non-Javadoc)
+	 * @see j.jave.framework.filedistribute.JDefaultLocalFileDistServiceConfigure#setJDefaultLocalFilePathStrategy(j.jave.framework.filedistribute.JDefaultLocalFilePathStrategy)
+	 */
+	@Override
+	public void setJDefaultLocalFilePathStrategy(
+			JDefaultLocalFilePathStrategy defaultLocalFilePathStrategy) {
+		this.jDefaultLocalFilePathStrategy=defaultLocalFilePathStrategy;
+	}
+
+	public String getLocalDirectory() {
+		return localDirectory;
+	}
+
+	public JDefaultLocalFilePathStrategy getDefaultLocalFilePathStrategy() {
+		return jDefaultLocalFilePathStrategy;
+	}
+
+	
+}

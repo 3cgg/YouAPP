4f11c75a33162bf47b4a1e90e4996f3a3721816a
jinwanmei <3cgg@163.com>
2016-01-31 00:36:07 +0800

    -

---
+++ youapp-platform/youapp-components-support/src/main/java/j/jave/framework/components/support/security/subhub/DESedeCipherServiceFactory.java
@@ -0,0 +1,46 @@
+package j.jave.framework.components.support.security.subhub;
+
+import j.jave.framework.commons.security.service.JDESedeCipherConfig;
+import j.jave.framework.commons.security.service.JDESedeCipherService;
+import j.jave.framework.commons.security.service.JDefaultDESedeCipherService;
+import j.jave.framework.commons.utils.JStringUtils;
+import j.jave.framework.components.core.servicehub.SpringServiceFactorySupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service(value="j.jave.framework.components.support.security.subhub.DESedeCipherServiceFactory")
+public class DESedeCipherServiceFactory extends SpringServiceFactorySupport<JDESedeCipherService> {
+
+	public DESedeCipherServiceFactory() {
+		super(JDESedeCipherService.class);
+	}
+
+	@Autowired(required=false)
+	private JDESedeCipherConfig desedeCipherConfig;
+	
+	private JDESedeCipherService desedeCipherService;
+	
+	private Object sync=new Object();
+	
+	@Override
+	public JDESedeCipherService getService() {
+		
+		if(desedeCipherService==null){
+			synchronized (sync) {
+				if(desedeCipherService==null){
+					if(desedeCipherConfig!=null&&JStringUtils.isNotNullOrEmpty(desedeCipherConfig.getKey())){
+						desedeCipherService=new JDefaultDESedeCipherService(desedeCipherConfig);
+					}
+					else{
+						desedeCipherService=new JDefaultDESedeCipherService();
+					}
+				}
+			}
+		}
+		
+		return desedeCipherService;
+	}
+	
+}

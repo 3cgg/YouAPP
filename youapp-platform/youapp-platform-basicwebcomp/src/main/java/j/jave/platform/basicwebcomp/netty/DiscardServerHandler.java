8527940c60da22f0460365ae701df2c7f2b9a907
J <3cgg@163.com>
2016-05-27 16:17:06 +0800

    -

--- youapp-platform/youapp-platform-basicwebcomp/src/main/java/j/jave/platform/basicwebcomp/netty/DiscardServerHandler.java
+++ youapp-platform/youapp-platform-basicwebcomp/src/main/java/j/jave/platform/basicwebcomp/netty/DiscardServerHandler.java
@@ -3,21 +3,15 @@ package j.jave.platform.basicwebcomp.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.ReferenceCountUtil;
 
-public class DiscardServerHandler implements ChannelHandler {
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> 
+	implements ChannelHandler {
 
 	@Override
 	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-		ByteBuf in = (ByteBuf) ctx.alloc().buffer();
-	    try {
-	        while (in.isReadable()) { // (1)
-	            System.out.print((char) in.readByte());
-	            System.out.flush();
-	        }
-	    } finally {
-	        ReferenceCountUtil.release(in); // (2)
-	    }
+		System.out.println("ha");
 	}
 	
 	@Override
@@ -28,7 +22,23 @@ public class DiscardServerHandler implements ChannelHandler {
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
 			throws Exception {
+		cause.printStackTrace();
 		System.out.println("e");
+	}
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		ByteBuf in = (ByteBuf) msg;
+	    try {
+	    	ReferenceCountUtil.retain(msg);
+	        while (in.isReadable()) { // (1)
+	            System.out.print((char) in.readByte());
+	            System.out.flush();
+	        }
+	    } finally {
+	        ReferenceCountUtil.release(in); // (2)
+	    }
 	}
 
 	
